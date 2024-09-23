281ec83f9fc088d333e175b5091fbcd9fe60e2a7
Patrick Reed <patrick.reed@wellsaidlabs.com>
2024-09-23 10:36:06 -0500

    Add simple clips support

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const AbortController = require('abort-controller');
 
-const ttsEndPoint = "https://api.wellsaidlabs.com/v1/tts/stream";
+const apiServer = "https://api.staging.wellsaidlabs.com/v1/tts"
+
+const ttsEndPoint = apiServer + "/stream";
+const clipsEndPoint = apiServer + "/clips"
 
 const app = express();
 app.use(bodyParser.json());
@@ -50,6 +53,78 @@ app.post('/stream', async (req, res) => {
 
   ttsResponse.body.pipe(res)
 });
+
+app.get('/clips', async (req, res) => {
+
+  clips = await fetch(apiServer+"/clips", {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    }
+  })
+
+  clips_obj = await clips.json()
+
+  res.send(clips_obj)
+})
+
+app.post('/create_clip', async (req, res) => {
+  const abortController = new AbortController();
+  const avatarId = req.body.avatarId;
+  const text = req.body.text;
+
+  req.on('aborted', () => {
+    // Graceful end of the TTS stream when a client connection is aborted
+    abortController.abort()
+  })
+
+  /**
+   * Should this request fail, make sure to check the response headers
+   * to try to find a root cause.
+   * 
+   * Rate-limiting headers:
+   * x-quota-limit: 200
+   * x-quota-remaining: 191
+   * x-quota-reset: 1622226323630
+   * x-rate-limit-limit: 5
+   * x-rate-limit-remaining: 4
+   * x-rate-limit-reset: 1619635874002
+   */
+  console.info(avatarId)
+  console.info(text)
+  const ttsResponse = await fetch(clipsEndPoint, {
+    signal: abortController.signal,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    },
+    body: JSON.stringify({
+      speaker_id: avatarId,
+      text,
+    }),
+  });
+  console.info(ttsResponse)
+  response_body = await ttsResponse.json()
+  console.info(response_body)
+  console.info("created clip" + response_body.clip_id)
+  res.send(response_body)
+});
+
+app.get("/clips/:id", async (req,res) => {
+  const clip_id =  req.params.id
+  console.info("want to get info for clip " +clip_id)
+  clips = await fetch(apiServer+"/clips/"+clip_id, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    }
+  })
+
+  clips_obj = await clips.json()
+
+  res.send(clips_obj)
+})
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
--- src/App.js
+++ src/App.js
@@ -23,42 +23,100 @@ function App() {
   const [currentAvatar, setCurrentAvatar] = useState(3);
   const [text, setText] = useState('');
   const [url, setUrl] = useState('');
+  const [createdClip, setCreatedClip] = useState(null)
 
-  const getClip = useCallback(async () => {
-    setUrl('');
-    const response = await fetch('/stream', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ avatarId: currentAvatar, text })
-      });
-    const responseBlob = await response.blob()
-    const objectURL = URL.createObjectURL(responseBlob);
-    setUrl(objectURL);
-  }, [currentAvatar, text])
+  // const getClip = useCallback(async () => {
+  //   setUrl('');
+  //   const response = await fetch('/stream', { 
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({ avatarId: currentAvatar, text })
+  //     });
+  //   const responseBlob = await response.blob()
+  //   const objectURL = URL.createObjectURL(responseBlob);
+  //   setUrl(objectURL);
+  // }, [currentAvatar, text])
+
+  const [clipsList, setClipsList] = useState([])
+
+  async function fetchClips() {
+
+    console.info("Fetching clips")
+    const clips_response= await fetch('/clips', {
+      method: 'GET',
+    })
+    console.info(clips_response.headers)
+    const clips = await clips_response.json()
+    console.info(clips)
+    setClipsList(clips);
+  }
+
+  async function createClip() {
+    console.log("create a clip")
+    const response = await fetch('/create_clip', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ avatarId: parseInt(currentAvatar), text })
+    });
+    const response_body = await response.json()
+    console.info("got response body")
+    console.info(response_body)
+    setCreatedClip({id:response_body.clip_id, status: "WAITING"})
+    // const responseBlob = await response.blob()
+    // const objectURL = URL.createObjectURL(responseBlob);
+    // setUrl(objectURL);
+  }
+
+  async function updateClipStatus() {
+    console.info("update clip please")
+    const response = await fetch('/clips/' + createdClip.id)
+
+    const body = await response.json()
+    console.info(body)
+    setCreatedClip(body)
+  }
 
   return (
-    <div className="App App-header">
-      <select 
-        className="input"
-        name='avatars'
-        value={currentAvatar}
-        onChange={({ target }) => { setCurrentAvatar(target.value); }}
-        style={{ marginBottom: 24 }}
-      >
-        {Avatars.map(avatar => (<option key={avatar.id} value={avatar.id}>{avatar.name}</option>))}
-      </select>
-      <textarea 
-        rows={8}
-        placeholder="Enter text here..."
-        value={text}
-        onChange={({ target }) => setText(target.value)}
-      />
-      <div style={{ marginBottom: url ? 24 : 0 }}>
-        {url && (<audio controls src={url} />)}
+    <div className="App App-header" >
+        <select 
+          className="input"
+          name='avatars'
+          value={currentAvatar}
+          onChange={({ target }) => { setCurrentAvatar(target.value); }}
+          style={{ marginBottom: 24 }}
+        >
+          {Avatars.map(avatar => (<option key={avatar.id} value={avatar.id}>{avatar.name}</option>))}
+        </select>
+        <textarea 
+          rows={8}
+          placeholder="Enter text here..."
+          value={text}
+          onChange={({ target }) => setText(target.value)}
+        />
+        {/* <div style={{ marginBottom: url ? 24 : 0 }}>
+          {url && (<audio controls src={url} />)}
+        </div> */}
+        <button className="input" onClick={createClip} disabled={text.trim().length == 0}>Create clip!</button>
+        <button className="input" onClick={fetchClips}>Fetch Clips</button>
+
+        { createdClip && (
+          <div>
+            Created Clip
+            <div> id: {createdClip.id} </div>
+            <div> status: {createdClip.status}</div>
+            {createdClip.status == "COMPLETE" && (<div style={{ marginBottom: url ? 24 : 0 }}>
+          <audio controls src={createdClip.url} />
+        </div>)}
+            <button onClick={updateClipStatus}>Update</button>
+          </div>
+          )}
+      <div>
+        {clipsList.map(clip => (<div>{clip.status}  {clip.id} {clip.status == "COMPLETE" && (<div style={{ marginBottom: clip.url ? 24 : 0 }}><audio controls src={clip.url} /></div>)}</div>))}
       </div>
-      <button className="input" onClick={getClip}>Get clip!</button>
     </div>
   );
 }
