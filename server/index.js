4b605249d506ab4ae459a69e2cecc2366ded5542
msargsyan98 <132940452+msargsyan98@users.noreply.github.com>
2025-01-08 15:29:51 -0500

    Added add library, add library replacement endpoint, and other endpoi… (#2)
    * Added add library, add library replacement endpoint, and other endpoints.
    
    * added get replacement library.
    
    * Added clips.
    
    * Addressed review comments.
    
    * Added get of overall libraries and respellings.
    
    * Minor changes.

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const AbortController = require('abort-controller');
 
-const ttsEndPoint = "https://api.wellsaidlabs.com/v1/tts/stream";
-const lookupEndpoint = "https://api.wellsaidlabs.com/v1/tts/respelling_suggestions?word=";
+const apiServer = "https://api.wellsaidlabs.com/v1/tts"
+const clipsEndPoint = apiServer + "/clips"
+const ttsEndPoint = apiServer + "/stream";
+const lookupEndpoint = apiServer + "/respelling_suggestions";
+const librariesEndpoint = apiServer + "/replacement_libraries";
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,12 +18,21 @@ app.post('/stream', async (req, res) => {
   const abortController = new AbortController();
   const avatarId = req.body.avatarId;
   const text = req.body.text;
+  const library = req.body.library;
 
   req.on('aborted', () => {
     // Graceful end of the TTS stream when a client connection is aborted
     abortController.abort()
   })
-
+  let body = library ? JSON.stringify({
+    speaker_id: avatarId,
+    text,
+    library_ids: library
+  }) : JSON.stringify({
+    speaker_id: avatarId,
+    text,
+  });
+  console.log(body)
   /**
    * Should this request fail, make sure to check the response headers
    * to try to find a root cause.
@@ -40,10 +52,7 @@ app.post('/stream', async (req, res) => {
       'Content-Type': 'application/json',
       'X-Api-Key': process.env.WELLSAID_API_KEY,
     },
-    body: JSON.stringify({
-      speaker_id: avatarId,
-      text,
-    }),
+    body
   });
   
   res.writeHead(ttsResponse.status, ttsResponse.headers.raw());
@@ -61,19 +70,7 @@ app.get('/respelling_suggestions', async (req, res) => {
     abortController.abort()
   })
 
-  /**
-   * Should this request fail, make sure to check the response headers
-   * to try to find a root cause.
-   * 
-   * Rate-limiting headers:
-   * x-quota-limit: 200
-   * x-quota-remaining: 191
-   * x-quota-reset: 1622226323630
-   * x-rate-limit-limit: 5
-   * x-rate-limit-remaining: 4
-   * x-rate-limit-reset: 1619635874002
-   */
-  const ttsResponse = await fetch(lookupEndpoint + word, {
+  const ttsResponse = await fetch(lookupEndpoint + "?word=" + word, {
     signal: abortController.signal,
     method: 'GET',
     headers: {
@@ -87,6 +84,196 @@ app.get('/respelling_suggestions', async (req, res) => {
 
   ttsResponse.body.pipe(res)
 });
+
+app.post('/replacement_libraries', async (req, res) => {
+  const abortController = new AbortController();
+  const name = req.body.name;
+
+  req.on('aborted', () => {
+    // Graceful end of the TTS stream when a client connection is aborted
+    abortController.abort()
+  })
+  
+  const ttsResponse = await fetch(librariesEndpoint, {
+    signal: abortController.signal,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    },    
+    body: JSON.stringify({
+      name,
+    }),
+  });
+  
+  res.writeHead(ttsResponse.status, ttsResponse.headers.raw());
+  res.flushHeaders();
+
+  ttsResponse.body.pipe(res)
+});
+
+app.get('/replacement_libraries', async (req, res) => {
+  const abortController = new AbortController();
+  const id = req.query.id;
+
+  req.on('aborted', () => {
+    // Graceful end of the TTS stream when a client connection is aborted
+    abortController.abort()
+  })
+  
+  let url = librariesEndpoint;
+  if (id) {
+    url += '/' + id;
+  }
+  const ttsResponse = await fetch(url, {
+    signal: abortController.signal,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    }
+  });
+  
+  res.writeHead(ttsResponse.status, ttsResponse.headers.raw());
+  res.flushHeaders();
+
+  ttsResponse.body.pipe(res)
+});
+
+app.post('/replacement_libraries/:id', async (req, res) => {
+  const abortController = new AbortController();
+  const id = req.params.id;
+  const original = req.body.original;
+  const replacement = req.body.replacement;
+  const is_phonetic_respelling = req.body.is_phonetic_respelling;
+  const enabled = req.body.enabled;
+
+  req.on('aborted', () => {
+    // Graceful end of the TTS stream when a client connection is aborted
+    abortController.abort()
+  })
+  
+  const ttsResponse = await fetch(librariesEndpoint + "/" + id + "/replacements", {
+    signal: abortController.signal,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    },    
+    body: JSON.stringify({
+      original,
+      replacement_text: replacement,
+      is_phonetic_respelling,
+      enabled
+    }),
+  });
+  
+  res.writeHead(ttsResponse.status, ttsResponse.headers.raw());
+  res.flushHeaders();
+
+  ttsResponse.body.pipe(res)
+});
+
+app.get('/replacement_libraries/:id/replacements/:replacement?', async (req, res) => {
+  const abortController = new AbortController();
+  const id = req.params.id;
+  const replacement = req.params.replacement;
+
+  req.on('aborted', () => {
+    // Graceful end of the TTS stream when a client connection is aborted
+    abortController.abort()
+  })
+  
+  let url = librariesEndpoint + "/" + id + "/replacements";
+  if (replacement) {
+    url += "/" + replacement;
+  }
+  const ttsResponse = await fetch(url, {
+    signal: abortController.signal,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    }
+  });
+  
+  res.writeHead(ttsResponse.status, ttsResponse.headers.raw());
+  res.flushHeaders();
+
+  ttsResponse.body.pipe(res)
+});
+
+app.get('/clips', async (req, res) => {
+  clips = await fetch(apiServer+"/clips", {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    }
+  })
+  clips_obj = await clips.json()
+  res.send(clips_obj)
+})
+app.post('/create_clip', async (req, res) => {
+  const abortController = new AbortController();
+  const avatarId = req.body.avatarId;
+  const text = req.body.text;
+  const library = req.body.library;
+
+  req.on('aborted', () => {
+    // Graceful end of the TTS stream when a client connection is aborted
+    abortController.abort()
+  })
+
+  let body = library ? JSON.stringify({
+    speaker_id: avatarId,
+    text,
+    library_ids: library
+  }) : JSON.stringify({
+    speaker_id: avatarId,
+    text,
+  });
+
+  /**
+   * Should this request fail, make sure to check the response headers
+   * to try to find a root cause.
+   * 
+   * Rate-limiting headers:
+   * x-quota-limit: 200
+   * x-quota-remaining: 191
+   * x-quota-reset: 1622226323630
+   * x-rate-limit-limit: 5
+   * x-rate-limit-remaining: 4
+   * x-rate-limit-reset: 1619635874002
+   */
+  console.info("avatarId: " + avatarId)
+  console.info("text: " + text)
+  const ttsResponse = await fetch(clipsEndPoint, {
+    signal: abortController.signal,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    },
+    body
+  });
+  console.info(ttsResponse)
+  response_body = await ttsResponse.json()
+  console.info(response_body)
+  console.info("created clip" + response_body.clip_id)
+  res.send(response_body)
+});
+app.get("/clips/:id", async (req,res) => {
+  const clip_id =  req.params.id
+  console.info("want to get info for clip " +clip_id)
+  clips = await fetch(clipsEndPoint + "/" + clip_id, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': process.env.WELLSAID_API_KEY,
+    }
+  })
+  clips_obj = await clips.json()
+  res.send(clips_obj)
+})
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
--- src/App.js
+++ src/App.js
@@ -22,9 +22,14 @@ const Avatars = [
 function App() {
   const [currentAvatar, setCurrentAvatar] = useState(3);
   const [text, setText] = useState('');
+  const [library, setLibrary] = useState();
+  const [isPhoneticRespellingEnabled, setIsPhoneticRespellingEnabled] = useState(false);
   const [url, setUrl] = useState('');
   const [endpoint, setEndpoint] = useState(3);
-  const [lookup, setLookup] = useState('');
+  const [original, setOriginal] = useState('');
+  const [replacement, setReplacement] = useState('');
+  const [response, setResponse] = useState('');
+  const [createdClip, setCreatedClip] = useState(null)
 
   const getClip = useCallback(async () => {
     setUrl('');
@@ -33,15 +38,108 @@ function App() {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ avatarId: currentAvatar, text })
+        body: JSON.stringify({ avatarId: currentAvatar, text, library })
       });
     const responseBlob = await response.blob()
     const objectURL = URL.createObjectURL(responseBlob);
     setUrl(objectURL);
-  }, [currentAvatar, text])
+  }, [currentAvatar, text, library])
+
+
+  const handleCheckboxChange = async () => {
+    setIsPhoneticRespellingEnabled(!isPhoneticRespellingEnabled);
+  }
+
+  const [clipsList, setClipsList] = useState([])
+  async function fetchClips() {
+    console.info("Fetching clips")
+    const clips_response= await fetch('/clips', {
+      method: 'GET',
+    })
+    console.info(clips_response.headers)
+    const clips = await clips_response.json()
+    console.info(clips)
+    setClipsList(clips);
+  }
+
+  async function createClip() {
+    console.log("create a clip")
+    const response = await fetch('/create_clip', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ avatarId: parseInt(currentAvatar), text, library })
+    });
+    const response_body = await response.json()
+    console.info("got response body")
+    console.info(response_body)
+    setCreatedClip({id:response_body.clip_id, status: "WAITING"})
+    // const responseBlob = await response.blob()
+    // const objectURL = URL.createObjectURL(responseBlob);
+    // setUrl(objectURL);
+  }
+
+  async function updateClipStatus() {
+    console.info("update clip please")
+    const response = await fetch('/clips/' + createdClip.id)
+    const body = await response.json()
+    console.info(body)
+    setCreatedClip(body)
+  }
+
+  const createLibrary = useCallback(async () => {
+    const response = await fetch('/replacement_libraries', { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: text })
+      });
+      const responseBody = JSON.stringify(await response.json())
+
+      setResponse(responseBody);
+  }, [text])
+
+  const getLibrary = useCallback(async () => {
+    const response = await fetch('/replacement_libraries?id=' + text, { 
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+      const responseBody = JSON.stringify(await response.json())
+
+      setResponse(responseBody);
+  }, [text])
+
+  const getLibraryRespelling = useCallback(async () => {
+    const response = await fetch('/replacement_libraries/' + text + '/replacements/' + replacement, { 
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const responseBody = JSON.stringify(await response.json())
+
+      setResponse(responseBody);
+  }, [text, replacement])
+
+  const addLibraryRespelling = useCallback(async () => {
+    const response = await fetch('/replacement_libraries/' + text, { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ original, replacement, is_phonetic_respelling : isPhoneticRespellingEnabled, enabled : true})
+      });
+      const responseBody = JSON.stringify(await response.json())
+
+      setResponse(responseBody);
+  }, [text, original, replacement, isPhoneticRespellingEnabled])
 
   const getLookup = useCallback(async () => {
-    const response = await fetch('respelling_suggestions?word=' + text, { 
+    const response = await fetch('respelling_suggestions?word='+ text, { 
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
@@ -49,8 +147,7 @@ function App() {
       });
     const responseBody = JSON.stringify(await response.json())
 
-    console.log(responseBody)
-    setLookup(responseBody);
+    setResponse(responseBody);
   }, [text])
 
   const renderLayout = () => {
@@ -73,6 +170,12 @@ function App() {
             value={text}
             onChange={({ target }) => setText(target.value)}
           />
+          <textarea 
+            rows={1}
+            placeholder="Enter library here..."
+            value={library}
+            onChange={({ target }) => setLibrary(target.value)}
+          />
           <div style={{ marginBottom: url ? 24 : 0 }}>
             {url && (<audio controls src={url} />)}
           </div>
@@ -81,25 +184,84 @@ function App() {
       case "Add library":
         return (
           <div className="App App-header">
-            <select 
-              className="input"
-              name='avatars'
-              value={currentAvatar}
-              onChange={({ target }) => { setCurrentAvatar(target.value); }}
-              style={{ marginBottom: 24 }}
-            >
-              {Avatars.map(avatar => (<option key={avatar.id} value={avatar.id}>{avatar.name}</option>))}
-            </select>
             <textarea 
-              rows={8}
-              placeholder="Enter text here..."
+              rows={1}
+              placeholder="Enter library name here..."
               value={text}
               onChange={({ target }) => setText(target.value)}
             />
-            <div style={{ marginBottom: url ? 24 : 0 }}>
-              {url && (<audio controls src={url} />)}
+            <div style={{ marginBottom: response ? 24 : 0 }}>
+              <p>{response}</p>
             </div>
-            <button className="input" onClick={getClip}>Get clip!</button>
+            <button className="input" onClick={createLibrary}>Create replacement library!</button>
+          </div>);
+      case "Get library":
+        return (
+          <div className="App App-header">
+            <textarea 
+              rows={1}
+              placeholder="Enter library id here..."
+              value={text}
+              onChange={({ target }) => setText(target.value)}
+            />
+            <div style={{ marginBottom: response ? 24 : 0 }}>
+              <p>{response}</p>
+            </div>
+            <button className="input" onClick={getLibrary}>Get library!</button>
+          </div>);
+      case 'Add library respelling':
+        return (
+          <div className="App App-header">
+            <textarea 
+              rows={1}
+              placeholder="Enter library id here..."
+              value={text}
+              onChange={({ target }) => setText(target.value)}
+            />
+            <textarea 
+              rows={1}
+              placeholder="Enter original here..."
+              value={original}
+              onChange={({ target }) => setOriginal(target.value)}
+            />
+            <textarea 
+              rows={1}
+              placeholder="Enter replacement here..."
+              value={replacement}
+              onChange={({ target }) => setReplacement(target.value)}
+            />
+            <label>
+              <input
+                type="checkbox"
+                checked={isPhoneticRespellingEnabled}
+                onChange={handleCheckboxChange}
+              />
+              is Phonetic Respelling Enabled
+            </label>
+            <div style={{ marginBottom: response ? 24 : 0 }}>
+              <p>{response}</p>
+            </div>
+            <button className="input" onClick={addLibraryRespelling}>Add library respelling!</button>
+          </div>);
+      case 'Get library respelling':
+        return (
+          <div className="App App-header">
+            <textarea 
+              rows={1}
+              placeholder="Enter library id here..."
+              value={text}
+              onChange={({ target }) => setText(target.value)}
+            />
+            <textarea 
+              rows={1}
+              placeholder="Enter respelling id here..."
+              value={replacement}
+              onChange={({ target }) => setReplacement(target.value)}
+            />
+            <div style={{ marginBottom: response ? 24 : 0 }}>
+              <p>{response}</p>
+            </div>
+            <button className="input" onClick={getLibraryRespelling}>Get library respelling!</button>
           </div>);
       case "Use Oxford Lookup":
         return (
@@ -110,11 +272,54 @@ function App() {
               value={text}
               onChange={({ target }) => setText(target.value)}
             />
-            <div style={{ marginBottom: lookup ? 24 : 0 }}>
-              <p>{lookup}</p>
+            <div style={{ marginBottom: response ? 24 : 0 }}>
+              <p>{response}</p>
             </div>
             <button className="input" onClick={getLookup}>Get respelling suggestions!</button>
           </div>);
+      case "Async Clips":
+        return (    <div className="App App-header" >
+          <select 
+            className="input"
+            name='avatars'
+            value={currentAvatar}
+            onChange={({ target }) => { setCurrentAvatar(target.value); }}
+            style={{ marginBottom: 24 }}
+          >
+            {Avatars.map(avatar => (<option key={avatar.id} value={avatar.id}>{avatar.name}</option>))}
+          </select>
+          <textarea 
+            rows={8}
+            placeholder="Enter text here..."
+            value={text}
+            onChange={({ target }) => setText(target.value)}
+          />
+          <textarea 
+            rows={1}
+            placeholder="Enter library here..."
+            value={library}
+            onChange={({ target }) => setLibrary(target.value)}
+          />
+          {/* <div style={{ marginBottom: url ? 24 : 0 }}>
+            {url && (<audio controls src={url} />)}
+          </div> */}
+          <button className="input" onClick={createClip} disabled={text.trim().length == 0}>Create clip!</button>
+          <button className="input" onClick={fetchClips}>Fetch Clips</button>
+          { createdClip && (
+            <div>
+              Created Clip
+              <div> id: {createdClip.id} </div>
+              <div> status: {createdClip.status}</div>
+              {createdClip.status == "COMPLETE" && (<div style={{ marginBottom: url ? 24 : 0 }}>
+            <audio controls src={createdClip.url} />
+          </div>)}
+              <button onClick={updateClipStatus}>Update</button>
+            </div>
+            )}
+        <div>
+          {clipsList.map(clip => (<div>{clip.status}  {clip.id} {clip.status == "COMPLETE" && (<div style={{ marginBottom: clip.url ? 24 : 0 }}><audio controls src={clip.url} /></div>)}</div>))}
+        </div>
+      </div>);
       default:
         return <div>Select a layout from the dropdown.</div>;
     }
@@ -128,7 +333,11 @@ function App() {
         onChange={(e) => setEndpoint(e.target.value)}
       >
         <option value="Stream">Stream</option>
+        <option value="Async Clips">Async Clips</option>
         <option value="Add library">Add library</option>
+        <option value="Get library">Get library</option>
+        <option value="Add library respelling">Add library respelling</option>
+        <option value="Get library respelling">Get library respelling</option>
         <option value="Use Oxford Lookup">Use Oxford Lookup</option>
       </select>
       <div className="layout-container">{renderLayout()}</div>
